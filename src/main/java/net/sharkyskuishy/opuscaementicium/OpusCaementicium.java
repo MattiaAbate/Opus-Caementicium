02161e4cf6b08b4356929e678f360903f5fee245
Mattia Abate <ab.mattia13@gmail.com>
2025-04-19 18:37:37 +0200

    Added "Mortar Bucket" item

--- src/main/java/net/sharkyskuishy/opuscaementicium/OpusCaementicium.java
+++ src/main/java/net/sharkyskuishy/opuscaementicium/OpusCaementicium.java
@@ -1,6 +1,7 @@
 package net.sharkyskuishy.opuscaementicium;
 
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -12,6 +13,7 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.sharkyskuishy.opuscaementicium.item.ModItems;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
@@ -31,6 +33,8 @@ public class OpusCaementicium {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -44,7 +48,9 @@ public class OpusCaementicium {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.MORTAR_BUCKET);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/sharkyskuishy/opuscaementicium/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.sharkyskuishy.opuscaementicium.item;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.sharkyskuishy.opuscaementicium.OpusCaementicium;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, OpusCaementicium.MOD_ID);
+
+    public static final RegistryObject<Item> MORTAR_BUCKET = ITEMS.register("mortar_bucket",
+        () -> new Item(new Item.Properties().setId(ResourceKey.create(Registries.ITEM, ResourceLocation.parse("opuscaementicium:mortar_bucket")))));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
